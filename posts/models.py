8f448d07be73e71e2fb1a26197c85fd642db27f4
taremwa <marktaremwa92@gmail.com>
2024-10-03 15:12:25 +0300

    users app | registration

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
-    banner = models.ImageField(default='fallback.png', blank=True)
+    banner = models.ImageField(default='fallback.jpg', blank=True)
 
     def __str__(self):
         return self.title
